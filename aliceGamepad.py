3eda438c7810af3ad7eb818cdb1c782d9eedf5a0
Duck <saheasy@gmail.com>
2021-09-16 20:02:47 -0500

    added servo gripper to Alice
    also reorganized the code a bit. Will have to wait to add the if statement.

--- aliceGamepad.py
+++ aliceGamepad.py
@@ -9,17 +9,15 @@ class robot:
     def __init__(self):
         self.servoLeft = 8
         self.servoRight = 9
+        self.servoGripper = 10
 
         self.board = pymata4.Pymata4()
         self.board.set_pin_mode_servo(self.servoLeft)
         self.board.set_pin_mode_servo(self.servoRight)
-
-    def drive(self):
-        self.board.servo_write(self.servoLeft, int(self.map(self.left, 0, 255, 175, 0)))
-        self.board.servo_write(self.servoRight, int(self.map(self.right, 0, 255, 0, 179)))
-
+        self.board.set_pin_mode_servo(self.servoGripper)
+    
     def run(self):
-        self.left, self.right = 87,89
+        self.left, self.right, self.gripper = 87,89, 0
         while 1:
             events = get_gamepad() #values 0-255 0 == max UP, 0 == max RIGHT
             for event in events:
@@ -27,9 +25,11 @@ class robot:
                     self.left = event.state
                 if event.code == "ABS_RZ":
                     self.right = event.state
-               
-            self.drive()
+                
+            self.board.servo_write(self.servoLeft, int(self.map(self.left, 0, 255, 175, 0)))
+            self.board.servo_write(self.servoRight, int(self.map(self.right, 0, 255, 0, 179)))
+            self.board.servo_write(self.servoGripper, int(self.map(self.gripper, 0, 255, 0, 180)))
 
 if __name__ == "__main__":
-    jerke = robot()
-    jerke.run()
+    alice = robot()
+    alice.run()
