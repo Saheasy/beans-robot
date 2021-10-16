e910f249a4f9a22d9907ff6510cbc905e5457634
Duck <saheasy@gmail.com>
2021-10-15 20:43:09 -0500

    added pin values

--- tjRobot.py
+++ tjRobot.py
@@ -13,7 +13,7 @@ class TrashJuniorRobot(robot):
                     self.board.servo_write(self.hardware['servo'], int(event.state))
                 if event.code == "ABS_RZ":
                     self.values["motor"] = event.state
-                    self.board.pwm_write( self.hardware['pwm_motor'],abs(int(value)-127)*2 )
+                    self.board.pwm_write( self.hardware['pwm_motor'],abs(int(event.state)-127)*2 )
                     if value >= 127:
                         self.board.digital_write(self.hardware['en_motor'],1)
                     if value < 127:
@@ -21,7 +21,7 @@ class TrashJuniorRobot(robot):
 if __name__ == "__main__":
     TJ = TrashJuniorRobot(
                  "beans", 
-                 {"digitalOutput": {"en_motor": 1 }, "pwmOutput": {"pwm_motor":0},"servo": {"servo":0}}, 
+                 {"digitalOutput": {"en_motor": 22 }, "pwmOutput": {"pwm_motor":8},"servo": {"servo":7}}, 
                  {"motor":0, "servo":0} )
     TJ.run()
     
