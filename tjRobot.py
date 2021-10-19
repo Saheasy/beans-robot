869d1e917adfa803651f854c1d56d0bbbdd74d3c
Duck <saheasy@gmail.com>
2021-10-18 23:02:12 -0500

    updated TJ hardware to match

--- tjRobot.py
+++ tjRobot.py
@@ -8,20 +8,24 @@ class TrashJuniorRobot(robot):
         
             events = get_gamepad()
             for event in events:
-                if event.code == "ABS_Z":
-                    self.values["servo"] = event.state
-                    self.board.servo_write(self.hardware['servo'], int(self.map(event.state, 0, 255, 0, 180)))
-                if event.code == "ABS_X":
-                    self.values["motor"] = event.state
-                    self.board.pwm_write( self.hardware['pwm_motor'],abs(int(event.state)-127)*2 )
+                if event.code == "ABS_RZ":
+                    self.values["right_motor"] = event.state
+                    self.board.pwm_write( self.hardware['pwm_right_motor'],abs(int(event.state)-127)*2 )
                     if event.state >= 127:
-                        self.board.digital_write(self.hardware['en_motor'],1)
+                        self.board.digital_write(self.hardware['en_right_motor'],1)
                     if event.state < 127:
-                        self.board.digital_write(self.hardware['en_motor'],0)
+                        self.board.digital_write(self.hardware['en_right_motor'],0)
+                if event.code == "ABS_Y":
+                    self.values["left_motor"] = event.state
+                    self.board.pwm_write( self.hardware['pwm_left_motor'],abs(int(event.state)-127)*2 )
+                    if event.state >= 127:
+                        self.board.digital_write(self.hardware['en_left_motor'],1)
+                    if event.state < 127:
+                        self.board.digital_write(self.hardware['en_left_motor'],0)
 if __name__ == "__main__":
     TJ = TrashJuniorRobot(
                  "beans", 
-                 {"digitalOutput": {"en_motor": 22 }, "pwmOutput": {"pwm_motor":8},"servo": {"servo":7}}, 
-                 {"motor":0, "servo":0} )
+                 {"digitalOutput": {"en_left_motor": 22,"en_right_motor": 23 }, "pwmOutput": {"pwm_left_motor":7,"pwm_right_motor":8 }}, 
+                 {"left_motor":0, "right_motor":0} )
     TJ.run()
     
